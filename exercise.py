939a69508742d7d2b809d1df3be6b5f3543798ae
David Seehaus <davidseehaus@mac.com>
2021-09-20 01:02:37 +0200

    Basic structure

---
+++ exercise.py
@@ -0,0 +1,23 @@
+import typing
+
+class Task:
+
+    def __init__(self) -> None:
+        pass
+
+    def answer(self, answer: float):
+        pass
+
+    def is_answered():
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+class Exercise:
+
+    def __init__(self, n_tasks: int) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
---
+++ player.py
@@ -0,0 +1,15 @@
+from exercise import Exercise
+
+class Player:
+
+    def __init__(self) -> None:
+        pass
+
+    def save_to_file(self, file: str) -> None:
+        pass
+
+    def add_exercise_to_history(self, exercise: Exercise) -> None:
+        pass
+
+def load_player_from_file(file: str) -> Player:
+    pass
